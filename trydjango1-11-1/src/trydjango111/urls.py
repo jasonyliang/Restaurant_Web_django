abd0c888d9b789d4d62384cc9f24e55f7ac1226d
Jason Liang <jjjasonliang@gmail.com>
2018-02-08 10:25:25 -0800

    Following feed
    For users that follows other users

--- trydjango1-11-1/src/trydjango111/urls.py
+++ trydjango1-11-1/src/trydjango111/urls.py
@@ -20,7 +20,7 @@ from django.views.generic.base import TemplateView
 from django.contrib.auth.views import LoginView, password_reset, PasswordResetDoneView
 
 from profiles.views import ProfileFollowToggle
-
+from menus.views import HomeView
 # from restaurants.views import (
 #     RestaurantListView,
 #     RestaurantDetailView,
@@ -30,7 +30,7 @@ from profiles.views import ProfileFollowToggle
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name = 'home.html'), name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^profile-follow/$', ProfileFollowToggle.as_view(), name='follow'),
     url(r'^password_reset/$', password_reset, name='password_reset'),
